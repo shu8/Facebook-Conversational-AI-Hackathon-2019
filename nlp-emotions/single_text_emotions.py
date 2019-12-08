ae539a1543258dbcf83f23bf44b6644bac36594f
gnggng <gng@itzamedia.com>
2019-12-08 11:16:29 +0000

    Added basic word highlighting

--- nlp-emotions/single_text_emotions.py
+++ nlp-emotions/single_text_emotions.py
@@ -57,92 +57,84 @@ negative positive negative positive positive \
 negative positive negative positive negative \
 negative positive negative positive negative \
 negative positive negative negative negative \
-:clap :eyes :gun :persevere :smiling_imp \
-:sweat :broken_heart :yellow_heart :musical_note :speak_no_evil \
-:wink :skull :confounded smile stuck_out_tongue_winking_eye \
-angry no_good muscle facepunch purple_heart \
-sparkling_heart blue_heart grimacing sparkles".split(' ')
+positive positive negative negative positive \
+negative negative negative positive positive \
+positive negative negative positive positive \
+negative negative positive positive positive \
+positive positive positive positive".split(' ')
 
 def top_elements(array, k):
     ind = np.argpartition(array, -k)[-k:]
     return ind[np.argsort(array[ind])][::-1]
 
+'''
+Return true if the sentence is positive, false if negative
+'''
+def positive_or_negative(emotions):
+    feelings = 0
+    print(f'emotion in function: {emotions}')
+    for emotion in emotions:
+        if emotion == 'positive':
+            feelings += 1
+        elif emotion == 'negative':
+            feelings -= 1
+    return 'positive' if feelings > 0 else 'negative'
+
 if __name__ == "__main__":
-    # argparser = argparse.ArgumentParser()
-    # argparser.add_argument('--text', type=str, required=True, help="Input text to emojize")
-    # argparser.add_argument('--maxlen', type=int, default=30, help="Max length of input text")
-    # args = argparser.parse_args()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--text', type=str, required=True, help="Input text to emojize")
+    argparser.add_argument('--maxlen', type=int, default=30, help="Max length of input text")
+    args = argparser.parse_args()
 
-    OUTPUT_PATH = 'test_sentences.csv'
-
-    with open('preprocessed-twitter-tweets/processedNeutral.csv', 'r') as f:
-        reader = csv.reader(f)
-        test_sentences = list(reader)
-
-    for sentence in test_sentences[0]:
-        if len(sentence) == 0:
-            sentence = 'i'
-
-    print(f'test_sentences length: {len(test_sentences[0])}')
-
-    # Tokenizing using dictionary
+    # Load dictionary for tokenizing
     with open(VOCAB_PATH, 'r') as f:
         vocabulary = json.load(f)
+    #print(f'vocabulary: {vocabulary}')
 
-    #st = SentenceTokenizer(vocabulary, args.maxlen)
-    st = SentenceTokenizer(vocabulary, 500)
+    with open('negative_words_parsed.txt', 'r', encoding='utf-8', errors='ignore') as negative_words_list:
+        negative_words = list(negative_words_list)
+        negative_words = [negative_word.rstrip('\n').lower() for negative_word in negative_words if negative_word != '\n']
+
+    with open('positive_words_parsed.txt', 'r') as positive_words_list:
+        positive_words = list(positive_words_list)
+        positive_words = [positive_word.rstrip('\n').lower() for positive_word in positive_words if positive_word != '\n']
+
+    st = SentenceTokenizer(vocabulary, args.maxlen)
 
     # Loading model
     model = torchmoji_emojis(PRETRAINED_PATH)
+
     # Running predictions
     # Determines the important words in the sentence
-    #tokenized, _, _ = st.tokenize_sentences([args.text])
-    tokenized, _, _ = st.tokenize_sentences(test_sentences[0])
-    #print(f'tokenized words: {tokenized}')
+    tokenized, _, _ = st.tokenize_sentences([args.text])
     # Get sentence probability
-    #prob = model(tokenized)[0]
-    print(f'tokenized: {tokenized}')
-    prob = model(tokenized)
+    prob = model(tokenized)[0]
 
-    for prob in [prob]:
-        # Find top emojis for each sentence. Emoji ids (0-63)
-        # correspond to the mapping in emoji_overview.png
-        # at the root of the torchMoji repo.
-        #print(f'prob:{prob}')
-        print('Writing results to {}'.format(OUTPUT_PATH))
-        scores = []
-        print(f'prob: {prob}')
-        for i, t in enumerate(test_sentences[0]):
-            t_tokens = tokenized[i]
-            t_score = [t]
-            t_prob = prob[i]
-            ind_top = top_elements(t_prob, 5)
-            t_score.append(sum(t_prob[ind_top]))
-            t_score.extend(ind_top)
-            t_score.extend([t_prob[ind] for ind in ind_top])
-            scores.append(t_score)
-            #print(t_score)
-
-        with open(OUTPUT_PATH, 'w') as csvfile:
-            writer = csv.writer(csvfile, delimiter=str(','), lineterminator='\n')
-            writer.writerow(['Text', 'Top5%',
-                            'Emoji_1', 'Emoji_2', 'Emoji_3', 'Emoji_4', 'Emoji_5',
-                            'Pct_1', 'Pct_2', 'Pct_3', 'Pct_4', 'Pct_5'])
-            for i, row in enumerate(scores):
-                try:
-                    writer.writerow(row)
-                except:
-                    print("Exception at row {}!".format(i))
-
-    '''
-    Single sentence mapping
     # Top emotion id
     emotion_ids = top_elements(prob, 5)
     print(f'top five emotion ids: {emotion_ids}')
 
     # map to emotions
     emotions = map(lambda x: EMOTIONS[x], emotion_ids)
-    print(f'emojis: {list(emotions)}')
-    #print(emotion.emojize("{} {}".format(args.text,' '.join(emojis)), use_aliases=True))
-    '''
+    emotions = list(emotions)
+    print(f'emotions: {emotions}')
+    user_feelings = positive_or_negative(emotions)
+    print(f'user_feelings: {user_feelings}')
+
+    # Find the words that are contributing to the feeling
+    user_positive_words = []
+    user_negative_words = []
+    if user_feelings == 'positive':
+        for word in args.text.split(' '):
+            for positive_word in positive_words:
+                if word == positive_word:
+                    user_positive_words.append(word) 
+        json_to_bot = {user_feelings: user_positive_words}
+    elif user_feelings == 'negative':
+        for word in args.text.split(' '):
+            for negative_word in negative_words:
+                if word == negative_word:
+                    user_negative_words.append(word)
+        json_to_bot = {user_feelings: user_negative_words}
+    print(f'json to bot: {json_to_bot}')
 
